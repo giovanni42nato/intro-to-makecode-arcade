175576fb62bfba8cf1750a2f9df502b1faab2568
giovanni42nato <142410244+giovanni42nato@users.noreply.github.com>
2023-08-16 08:43:27 -0400

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,10 +1,11 @@
 controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
-	
+    mySprite.startEffect(effects.spray, randint(100, 600))
 })
+let mySprite: Sprite = null
 game.splash("Something more exciting", "Splash")
 scene.setBackgroundColor(0)
 scene.setBackgroundColor(5)
-let mySprite = sprites.create(img`
+mySprite = sprites.create(img`
     . . . . . . . . . . . . . . . . 
     . . . . . . . . . . . . . . . . 
     . . . . . . . . . . . . . . . . 
